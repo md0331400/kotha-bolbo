13b5c7d7b8db6fb25b1d117da729b5a4efdeeacf
md0331400 <122189051+md0331400@users.noreply.github.com>
2026-05-17 04:04:06 +0600

    Add files via upload

--- api/imagekit-auth.js
+++ api/imagekit-auth.js
@@ -1,1 +1,14 @@
+const ImageKit = require('imagekit');
 
+const ik = new ImageKit({
+  publicKey: process.env.IK_PUBLIC_KEY,
+  privateKey: process.env.IK_PRIVATE_KEY,
+  urlEndpoint: process.env.IK_URL_ENDPOINT,
+});
+
+module.exports = function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  const result = ik.getAuthenticationParameters();
+  res.status(200).json(result);
+};
